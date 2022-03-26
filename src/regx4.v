33663f125260027307127a6108f1839f067231bb
Kevin Kwak <34068953+kevinkwak@users.noreply.github.com>
2022-03-25 23:53:34 -0700

    Two register files

--- src/regx4.v
+++ src/regx4.v
@@ -18,10 +18,8 @@ module regx4 #(
 	output reg [D_WIDTH-1:0] reg2data
 
 );
-    wire [4:0] reg_wraddr1;
-    wire [4:0] reg_wraddr2;
-    wire [4:0] reg_wraddr3;
-    wire [4:0] reg_wraddr4;
+    wire [5:0] reg_wraddr1;
+    wire [5:0] reg_wraddr2;
 
     reg [3:0]  thread_sel_ID_r;
 	
@@ -31,17 +29,10 @@ module regx4 #(
 	wire [D_WIDTH-1:0] reg1data_file2;
 	wire [D_WIDTH-1:0] reg2data_file2;
 
-	wire [D_WIDTH-1:0] reg1data_file3;
-	wire [D_WIDTH-1:0] reg2data_file3;
-
-	wire [D_WIDTH-1:0] reg1data_file4;
-	wire [D_WIDTH-1:0] reg2data_file4;
-
-
-   assign reg_wraddr1 = (ctrl_WB & thread_sel_WB[0]) ? reg_wraddr : rs1_ID;
+   assign reg_wraddr1 = (ctrl_WB & (thread_sel_WB[0] | thread_sel_WB[2])) ? {thread_sel_WB[2], reg_wraddr} : {thread_sel_ID[2], rs1_ID}; //thread 1 0-31 or 3 32-63
    reg_file inst_reg_file_1 (
         .addra(reg_wraddr1), 
-        .addrb(rs2_ID), 
+        .addrb({thread_sel_ID[2], rs2_ID}), 
         .clka(clk), 
         .clkb(clk), 
         .dina(data_WB), 
@@ -52,10 +43,10 @@ module regx4 #(
         .doutb(reg2data_file1)
 	);
 
-   assign reg_wraddr2 = (ctrl_WB & thread_sel_WB[1]) ? reg_wraddr : rs1_ID;
+   assign reg_wraddr2 = (ctrl_WB & (thread_sel_WB[1] | thread_sel_WB[3])) ? {thread_sel_WB[3], reg_wraddr} : {thread_sel_ID[3], rs1_ID}; //thread 2 or 4
    reg_file inst_reg_file_2 (
         .addra(reg_wraddr2), 
-        .addrb(rs2_ID), 
+        .addrb({thread_sel_ID[3], rs2_ID}), 
         .clka(clk), 
         .clkb(clk), 
         .dina(data_WB), 
@@ -64,34 +55,6 @@ module regx4 #(
         .web(1'b0), 
         .douta(reg1data_file2), 
         .doutb(reg2data_file2)
-	);
-	
-   assign reg_wraddr3 = (ctrl_WB & thread_sel_WB[2]) ? reg_wraddr : rs1_ID;
-   reg_file inst_reg_file_3 (
-        .addra(reg_wraddr3), 
-        .addrb(rs2_ID), 
-        .clka(clk), 
-        .clkb(clk), 
-        .dina(data_WB), 
-        .dinb(), 
-        .wea(ctrl_WB & thread_sel_WB[2]), 
-        .web(1'b0), 
-        .douta(reg1data_file3), 
-        .doutb(reg2data_file3)
-	);
-	
-   assign reg_wraddr4 = (ctrl_WB & thread_sel_WB[3]) ? reg_wraddr : rs1_ID;
-   reg_file inst_reg_file_4 (
-        .addra(reg_wraddr4), 
-        .addrb(rs2_ID), 
-        .clka(clk), 
-        .clkb(clk), 
-        .dina(data_WB), 
-        .dinb(), 
-        .wea(ctrl_WB & thread_sel_WB[3]), 
-        .web(1'b0), 
-        .douta(reg1data_file4), 
-        .doutb(reg2data_file4)
 	);
 	
 	always @(posedge clk)
@@ -108,12 +71,12 @@ module regx4 #(
                 reg2data = reg2data_file2;
             end
             4'b0100: begin
-                reg1data = reg1data_file3;
-                reg2data = reg2data_file3;
+                reg1data = reg1data_file1;
+                reg2data = reg2data_file1;
             end
             4'b1000: begin
-                reg1data = reg1data_file4;
-                reg2data = reg2data_file4;
+                reg1data = reg1data_file2;
+                reg2data = reg2data_file2;
             end
 			default: begin
                 reg1data = 'h0;
